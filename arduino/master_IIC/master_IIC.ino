b72c0f6d39b977111643cd021ca10b46eba4f157
Unknown <n-i-k-i-t@yandex.ru>
2018-07-11 17:16:23 +0300

    Redacted 'Master_IIC'
    Updated 'player_settings' void.

--- arduino/master_IIC/master_IIC.ino
+++ arduino/master_IIC/master_IIC.ino
@@ -43,21 +43,21 @@ void loop()
 
 	Wire.requestFrom(L_ADRESS, 1);    // requesting byte from device #1
 	L = Wire.read(); // receive byte as character
-	Serial.println("L " + String(L));         // print the character
+	//Serial.println("L " + String(L));         // print the character
 	L = map (L, 0, 170, 30, 0);
 
 	Wire.requestFrom(C_ADRESS, 1);    // requesting byte from device #2
 	C = Wire.read(); // receive a byte as character
-	Serial.println("C " + String(C));         // print the character
+	//Serial.println("C " + String(C));         // print the character
 	C = map (C, 0, 170, 30, 0);
 
 	Wire.requestFrom(R_ADRESS, 1);    // requesting byte from device #3
 	R = Wire.read(); // receive a byte as character
-	Serial.println("R " + String(R));         // print the character
+	//Serial.println("R " + String(R));         // print the character
 	R = map (R, 0, 170, 30, 0);
 
   // New line
-  Serial.println();
+  //Serial.println();
 
 
 /* Playing Sounds */
@@ -77,13 +77,13 @@ void player_settings()
 {
 	music_player.begin (9600); // opening soft-port
 		
-	Serial.println(F("\nInitializing DFPlayer\nThis operation may take some time . . ."));
+	Serial.println(F("\nInitializing DFPlayer\nThis operation may take some time . . .\n"));
 
-	if (!myDFPlayer.begin(music_player)) 
+	if (myDFPlayer.begin(music_player)) 
 	{  // Using softwareSerial to communicate with mp3.
-		Serial.println(F("Unable to begin. Possible Reasons:\n "));
+		Serial.println(F("Unable to begin. Possible Reasons: "));
 		Serial.println(F("1. Connection lost."));
-		Serial.println(F("2. SD card may not be inserted yet."));
+		Serial.println(F("2. SD card may not be inserted yet.\n"));
 	}
-	Serial.println(F("DFPlayer Mini online."));
+	else{Serial.println(F("DFPlayer Mini online."));}
 }
